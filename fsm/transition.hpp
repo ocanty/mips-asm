d1ad10de0b38c276f25ab8cc55e8166ae4516b32
ocanty <git@ocanty.com>
2019-01-17 11:07:18 +0000

    Better func type naming

--- fsm/transition.hpp
+++ fsm/transition.hpp
@@ -13,6 +13,18 @@ template <typename States, typename InputsType>
 class transition {
 public:
     /**
+     * The condition checking function, the input is supplied,
+     * Process it as you wish and return true to let a transition occur/be valid
+     * else return false
+     */
+    using should_transition_func    = std::function<bool(InputsType&)>;
+
+    /**
+     * A callback function for when the transition occurs
+     */
+    using transition_callback_func  = std::function<void(InputsType&)>;
+
+    /**
      * Defines a transition between one state to another
      *
      * @param leaving_state             The state we can leave
@@ -24,13 +36,13 @@ public:
     transition(
         const States& leaving_state,
         const States& entering_state,
-        std::function<bool(const InputsType&)> should_transition_func,
-        std::function<void(const InputsType&)> transition_callback_func
+        should_transition_func should_transition,
+        transition_callback_func transition_callback
     ) :
         m_start_state(leaving_state),
         m_finish_state(entering_state),
-        m_should_transition_func(should_transition_func),
-        m_transition_callback_func(transition_callback_func)
+        m_should_transition_func(should_transition),
+        m_transition_callback_func(transition_callback)
     {
 
     }
@@ -46,11 +58,11 @@ public:
      transition(
         const States& leaving_state,
         const States& entering_state,
-        std::function<bool(const InputsType&)> should_transition_func
+        should_transition_func should_transition
     ) :
         m_start_state(leaving_state),
         m_finish_state(entering_state),
-        m_should_transition_func(should_transition_func),
+        m_should_transition_func(should_transition),
         m_transition_callback_func([](){})
     {
 
@@ -84,7 +96,10 @@ public:
         return m_should_transition_func(inputs);
     }
 
-    std::function<void(const InputsType&)> get_transition_callback() const {
+    /**
+     * @return Get the transition callback
+     */
+    transition_callback_func get_transition_callback() const {
         return m_transition_callback_func;
     }
 
@@ -103,12 +118,12 @@ private:
      * A function that returns true when the value of the input type warrants
      * the current state to transition to a new one
      */
-    std::function<bool(const InputsType&)> m_should_transition_func;
+    should_transition_func m_should_transition_func;
 
     /**
      * A function that will be called when the state transitions
      */
-    std::function<void(const InputsType&)> m_transition_callback_func;
+    transition_callback_func m_transition_callback_func;
 };
 
 }
